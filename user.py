4de4c582a2392ed9a5062d664e4a61bd1e12f4f0
KungPaoChick <matthewriel63@gmail.com>
2021-03-01 14:51:43 +0000

    🐍 Added few changes

--- user.py
+++ user.py
@@ -1,23 +1,29 @@
 import json
-import dl_api
+import mk_json
 import time
 
 
-def get_user(json_user):
-    for info in json_user:
-        name = '{} {} {}'.format(info['name']['title'],
-                                 info['name']['first'],
-                                 info['name']['last'])
-        age = info['dob']['age']
-        country = info['location']['country']
+class my_json:
 
-    print(f"Hello, I'm {name}, and I'm {age} years old. I live in {country}")
+    def __init__(self, json_user):
+        self.json_user = json_user
+
+
+    def get_user(self):
+        for info in self.json_user:
+            name = '{} {} {}'.format(info['name']['title'],
+                                    info['name']['first'],
+                                    info['name']['last'])
+            age = info['dob']['age']
+            country = info['location']['country']
+
+        print(f"Hello, I'm {name}, and I'm {age} years old. I live in {country}")
 
 
 if __name__ == '__main__':
     while True:
-        dl_api.random_user_api()
+        mk_json.random_user_api()
         with open('random_user.json') as f:
             data = json.load(f)
-            get_user(data)
-        time.sleep(3)
+            my_json(data).get_user()
+        time.sleep(1.5)
